b716d6ed094d4325c20b5f98d9719943e826b968
Alexander Kehr <alexander.kehr@mgm-tp.com>
2023-03-26 22:31:29 +0200

    implement error boundary

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { AppHeader } from "./app-header";
 import { Router } from "./router";
+import { ErrorBoundary } from "react-error-boundary";
+import { ErrorScreen } from "./error-screen";
 
 export function App() {
   return (
-    <React.StrictMode>
-      <AppHeader />
-      <Router />
-    </React.StrictMode>
+    <ErrorBoundary fallback={<ErrorScreen />}>
+      <React.StrictMode>
+        <AppHeader />
+        <Router />
+      </React.StrictMode>
+    </ErrorBoundary>
   );
 }
