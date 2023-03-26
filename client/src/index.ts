160c0d6f5396fb554ae384e33885ba5ae920f756
Alexander Kehr <alexander.kehr@mgm-tp.com>
2023-03-26 22:32:06 +0200

    switch to createRoot

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,5 +1,5 @@
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import { createElement } from "react";
 import { App } from "./app";
 
-render(createElement(App), document.getElementById("app"));
+createRoot(document.getElementById("app")!).render(createElement(App));
