6e5532d7d03f94e37c77f47679b9711cb30e95e7
Alexander Kehr <alex@alexanderkehr.de>
2023-03-27 17:07:34 +0200

    implement delete note

--- client/src/memo/memo-list-screen.tsx
+++ client/src/memo/memo-list-screen.tsx
@@ -1,16 +1,20 @@
-import React from "react";
-import { useMemoStore } from "./memo-store";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { useMemoStore } from './memo-store';
+import { useNavigate } from 'react-router-dom';
 
 export function MemoListScreen() {
-  const { memos } = useMemoStore();
+  const navigate = useNavigate();
+  const { memos, deleteMemo } = useMemoStore();
   return (
     <div>
       <h1>Memos</h1>
       <ul>
         {memos.map((memo) => (
           <li key={memo.id}>
-            {memo.content}&nbsp;<Link to={`/note/${memo.id}`}>edit</Link>
+            {memo.content}&nbsp;
+            <button onClick={() => navigate(`/note/${memo.id}`)}>edit</button>
+            &nbsp;
+            <button onClick={() => deleteMemo(memo.id)}>delete</button>
           </li>
         ))}
       </ul>
