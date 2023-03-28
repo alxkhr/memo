b50e33f1bbe15d5ea19985f71b455c2e26647b61
Alexander Kehr <alex@alexanderkehr.de>
2023-03-28 18:03:00 +0200

    implement client db for memos

---
+++ client/src/memo/memo-db.ts
@@ -0,0 +1,61 @@
+import { Memo } from './memo';
+
+type StoredMemo = Memo & { deleted: boolean };
+
+const DB_NAME = 'memo';
+const STORE_NAME = 'memo';
+const STORE_KEY_PATH = 'id';
+const DB_VERSION = 1;
+
+function connect(): Promise<IDBDatabase> {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(DB_NAME, DB_VERSION);
+    request.onupgradeneeded = (event) => {
+      const db = (
+        event as IDBVersionChangeEvent & { target: { result: IDBDatabase } }
+      ).target.result;
+      db.createObjectStore(STORE_NAME, { keyPath: STORE_KEY_PATH });
+    };
+    request.onsuccess = (event) => {
+      const db = (event as Event & { target: { result: IDBDatabase } }).target
+        .result;
+      resolve(db);
+    };
+    request.onerror = (e) => reject(e);
+  });
+}
+
+export async function storeMemo(memo: Memo) {
+  const db = await connect();
+  const transaction = db.transaction(STORE_NAME, 'readwrite');
+  const store = transaction.objectStore(STORE_NAME);
+  store.put({ ...memo, deleted: false });
+}
+
+export async function getMemos(): Promise<Memo[]> {
+  const db = await connect();
+  const transaction = db.transaction(STORE_NAME, 'readonly');
+  const store = transaction.objectStore(STORE_NAME);
+  const request = store.getAll();
+  return new Promise((resolve, reject) => {
+    request.onsuccess = (event) => {
+      const memos = (event as Event & { target: { result: StoredMemo[] } })
+        .target.result;
+      resolve(memos.filter((memo) => !memo.deleted));
+    };
+    request.onerror = (e) => reject(e);
+  });
+}
+
+export async function deleteMemo(id: string) {
+  const db = await connect();
+  const transaction = db.transaction(STORE_NAME, 'readwrite');
+  const store = transaction.objectStore(STORE_NAME);
+  const request = store.get(id);
+  request.onsuccess = (event) => {
+    const memo = (event as Event & { target: { result: StoredMemo } }).target
+      .result;
+    memo.deleted = true;
+    store.put(memo);
+  };
+}
--- client/src/memo/memo-store.ts
+++ client/src/memo/memo-store.ts
@@ -1,5 +1,7 @@
-import { create } from "zustand";
-import { Memo } from "./memo";
+import { useStore } from 'zustand';
+import { createStore } from 'zustand/vanilla';
+import { Memo } from './memo';
+import { deleteMemo, storeMemo, getMemos } from './memo-db';
 
 interface MemoStore {
   memos: Memo[];
@@ -8,19 +10,32 @@ interface MemoStore {
   updateMemo: (memo: Memo) => void;
 }
 
-export const useMemoStore = create<MemoStore>((set) => ({
+const memoStore = createStore<MemoStore>((set) => ({
   memos: [],
+  setMemos: (memos: Memo[]) => set({ memos }),
   addMemo: (memo: Memo) => {
+    storeMemo(memo);
     set((state) => ({ memos: [...state.memos, memo] }));
   },
   deleteMemo: (id: string) => {
+    deleteMemo(id);
     set((state) => ({
       memos: state.memos.filter((memo) => memo.id !== id),
     }));
   },
   updateMemo: (memo: Memo) => {
+    storeMemo(memo);
     set((state) => ({
       memos: state.memos.map((m) => (m.id === memo.id ? memo : m)),
     }));
   },
 }));
+
+(async function () {
+  const memos = await getMemos();
+  memoStore.setState({ memos });
+})();
+
+export function useMemoStore() {
+  return useStore(memoStore);
+}
