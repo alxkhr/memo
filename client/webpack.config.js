dc7ecf13b8a40daef2fe15ec5550190714ce509b
Alexander Kehr <alex@alexanderkehr.de>
2023-04-06 18:08:42 +0200

    setup css modules

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,44 @@
-const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode = process.env.NODE_ENV || 'production';
+const styleLoader =
+  mode === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader;
 
 module.exports = {
-  mode: process.env.NODE_ENV || "production",
-  entry: "./client/src/index.ts",
+  mode,
+  entry: './client/src/index.ts',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: {
+          loader: 'ts-loader',
+          options: { transpileOnly: true },
+        },
         exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          styleLoader,
+          { loader: 'css-loader', options: { modules: true } },
+        ],
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
     new CopyPlugin({
-      patterns: ["client/res/index.html"],
+      patterns: ['client/res/index.html'],
     }),
   ],
 };
