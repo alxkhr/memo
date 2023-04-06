ce6fc961546c125682b9cdeb46200cfafe74781c
Alexander Kehr <alex@alexanderkehr.de>
2023-04-06 22:16:41 +0200

    implement search in list

--- client/src/memo/memo-list-screen.tsx
+++ client/src/memo/memo-list-screen.tsx
@@ -5,14 +5,29 @@ import { useNavigate } from 'react-router-dom';
 export function MemoListScreen() {
   const navigate = useNavigate();
   const { memos, removeMemo } = useMemoStore();
+  const [search, setSearch] = React.useState('');
   if (!memos) {
     return <div>Loading...</div>; // TODO loading component
   }
+  const searchSplit = search.split(' ');
+  const filteredMemos =
+    search === ''
+      ? memos
+      : memos.filter((memo) =>
+          searchSplit.every((s) =>
+            memo.content.toLowerCase().includes(s.toLowerCase()),
+          ),
+        );
   return (
     <div>
       <h1>Memos</h1>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <ul>
-        {memos.map((memo) => (
+        {filteredMemos.map((memo) => (
           <li key={memo.id}>
             {memo.content}&nbsp;
             <button onClick={() => navigate(`/note/${memo.id}`)}>edit</button>
