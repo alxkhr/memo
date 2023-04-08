fcc8d4b87506c925811c654b65feb46f2394a7aa
Alexander Kehr <alex@alexanderkehr.de>
2023-04-08 23:09:05 +0200

    implement global element styles

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,5 +1,6 @@
-import { createRoot } from "react-dom/client";
-import { createElement } from "react";
-import { App } from "./app";
+import { createRoot } from 'react-dom/client';
+import { createElement } from 'react';
+import { App } from './app';
+import './global.m.css';
 
-createRoot(document.getElementById("app")!).render(createElement(App));
+createRoot(document.getElementById('app')!).render(createElement(App));
