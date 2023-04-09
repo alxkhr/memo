3dea65f3614d1ac269c2cd4b42be0ab751eaa19f
Alexander Kehr <alex@alexanderkehr.de>
2023-04-09 23:05:49 +0200

    implement header styles

--- client/src/frame/app-header.tsx
+++ client/src/frame/app-header.tsx
@@ -1,21 +1,33 @@
-import React from "react";
-import { useNavigate, Link } from "react-router-dom";
-import { generateMemoId } from "../memo/generate-memo-id";
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { generateMemoId } from '../memo/generate-memo-id';
+import css from './app-header.m.css';
 
-export function AppHeader() {
+export function AppHeader(props: { className?: string }) {
   const navigate = useNavigate();
   function onClickNew(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
     navigate(`/note/${generateMemoId()}`);
   }
+  const classNames = [css.container];
+  if (props.className) {
+    classNames.push(props.className);
+  }
   return (
-    <div>
-      <h1>Headnut</h1>
-      <p>
-        <Link to="/">Notes</Link>&nbsp;|&nbsp;
-        <Link to="/connect">Connect</Link>&nbsp;|&nbsp;
-        <button onClick={onClickNew}>New</button>
-      </p>
+    <div className={classNames.join(' ')}>
+      <Link className={css.link} to="/">
+        search
+      </Link>
+      <Link className={css.link} to="/connect">
+        connect
+      </Link>
+      <h1 className={css.title}>Headnut</h1>
+      <button className={css.new} onClick={onClickNew}>
+        <FontAwesomeIcon icon={faPlus} />
+        &nbsp;NEW
+      </button>
     </div>
   );
 }
